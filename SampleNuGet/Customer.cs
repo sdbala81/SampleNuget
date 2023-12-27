dedb73acb4aedc2c3084009fa6f89c2f543f25d5
Dhananjeyan Balaretnaraja <sdbala@gmail.com>
2023-12-27 09:22:27 +0530

    Removed Return Models
    Added customer class with FirstName and LastName properties

---
+++ SampleNuGet/Customer.cs
@@ -0,0 +1,27 @@
+﻿namespace SampleNuGet;
+
+/// <summary>
+/// Customer Entity Class
+/// </summary>
+public class Customer
+{
+    /// <summary>
+    /// Firstname of the customer
+    /// </summary>
+    public string FirstName { get; set; }
+    
+    /// <summary>
+    /// Lastname of the customer
+    /// </summary>
+    public string LastName { get; set; }
+    
+    /// <summary>
+    /// Method to get full name
+    /// </summary>
+    /// <returns>Returns Fullname</returns>
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+    
+}
