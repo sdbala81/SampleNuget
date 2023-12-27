ead069ec39f9671181b1ac428ebdc400247b3b18
Dhananjeyan Balaretnaraja <sdbala@gmail.com>
2023-12-27 10:02:56 +0530

    Added Custom Serialization

---
+++ CustomSerializer/IJsonDeserializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace QT.OBE.CustomSerializer;
+
+/// <summary>
+/// Interface for Json Deserializer
+/// </summary>
+public interface IJsonDeserializer
+{
+    /// <summary>
+    /// Deserialize the json string to object
+    /// </summary>
+    JsonSerializerOptions SerializerOptions { get; set; }
+
+    
+}
---
+++ CustomSerializer/JsonDeserializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace QT.OBE.CustomSerializer;
+
+
+/// <summary>
+/// Interface for Json Deserializer
+/// </summary>
+public class JsonDeserializer : IJsonDeserializer
+{
+    
+    /// <summary>
+    /// Deserialize the json string to object
+    /// </summary>
+    public JsonSerializerOptions SerializerOptions { get; set; } = new(JsonSerializerDefaults.Web)
+    {
+        DefaultBufferSize = 10,
+        PropertyNameCaseInsensitive = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    
+}
